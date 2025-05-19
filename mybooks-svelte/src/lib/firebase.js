34a52bde331d6e274c549a4d0a009f50ec9947fe
Pablomaluk <pablo.maluk@uc.cl>
2025-05-19 13:33:07 -0400

    Config Svelte + Firebase
    Proyecto Svelte configurado. Firebase creado y funcionando conectado a la app

---
+++ mybooks-svelte/src/lib/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgLlLgdGE7Y3IBxJAyMbVnA6i9aBUIIdA",
+  authDomain: "mybooks-e4a30.firebaseapp.com",
+  projectId: "mybooks-e4a30",
+  storageBucket: "mybooks-e4a30.firebasestorage.app",
+  messagingSenderId: "421221868497",
+  appId: "1:421221868497:web:f323c4151ba533ca2af6f0"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
---
+++ readme.md
@@ -0,0 +1,6 @@
+Pasos para correr Svelte:
+
+Actualizar Node y NVM.
+Entrar a mybooks-svelte y correr:
+- npm install
+- npm run dev
